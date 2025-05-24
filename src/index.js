02f8b43a28f4e5118e5e49b1d473a9bb06088a50
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2025-05-24 22:55:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -70,9 +70,68 @@ app.post('/upload-from-url', async (req, res) => {
     }
 });
 
+
+
+const DID = "7506797301241120274";
+let VIDEO = "7477537590149713170";
+
+async function view(video) {
+    try {
+        const version = [247, 312, 322, 357, 358, 415, 422, 444, 466][Math.floor(Math.random() * 9)];
+        const device = ["SM-G9900", "sm-g950f", "SM-A136U1", "SM-M225FV", "SM-E426B", "SM-M526BR", "SM-M326B", "SM-A528B", "SM-F711B", "SM-F926B", "SM-A037G", "SM-A225F", "SM-M325FV", "SM-A226B", "SM-M426B", "SM-A525F"][Math.floor(Math.random() * 16)];
+        const host = ["api16.tiktokv.com", "api.tiktokv.com", "api19.tiktokv.com", "api21.tiktokv.com"][Math.floor(Math.random() * 4)];
+
+        const params = querystring.stringify({
+            app_language: "fr",
+            iid: "",
+            device_id: DID,
+            channel: "googleplay",
+            device_type: device,
+            ac: "wifi",
+            os_version: Math.floor(Math.random() * (11 - 5 + 1)) + 5, // Random từ 5-11
+            version_code: version,
+            app_name: "trill",
+            device_brand: "samsung",
+            ssmix: "a",
+            device_platform: "android",
+            aid: 1180,
+            as: "a1iosdfgh", // creds to @auut for params bypass
+            cp: "androide1",
+        });
+
+        const data = `&manifest_version_code=${version}&update_version_code=${version}0&play_delta=1&item_id=${video}&version_code=${version}&aweme_type=0`;
+
+        const response = await axios.post(
+            `https://api21.tiktokv.com/aweme/v1/aweme/stats?app_language=fr&iid=&device_id=7506797301241120274&channel=googleplay&device_type=SM-F711B&ac=wifi&os_version=7&version_code=357&app_name=trill&device_brand=samsung&ssmix=a&device_platform=android&aid=1180&as=a1iosdfgh&cp=androide1`,
+            data,
+            {
+                headers: {
+                    "host": "api21.tiktokv.com",
+                    "connection": "keep-alive",
+                    "accept-encoding": "gzip",
+                    "x-ss-req-ticket": Math.floor(Date.now() / 1000).toString(),
+                    "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                    "user-agent": `com.ss.android.ugc.trill/${version} (Linux; U; Android 11; fr_FR; ${device}; Build/RP1A.200720.012; Cronet/58.0.2991.0)`
+                },
+            }
+        );
+
+        const timestamp = new Date().toLocaleTimeString('en-US', { hour12: false });
+        console.log(
+            `${`[${timestamp}]`} ` +
+            `${'[VIEW]'} ` +
+            `${`==> send +1 view to ${VIDEO}`}`
+        );
+    } catch (error) {
+        console.log(error)
+        // Bỏ qua lỗi, tương tự Python
+    }
+}
+
 // Route để xem video
 app.get('/videos/:videoName', (req, res) => {
     const videoName = req.params.videoName;
+    view(videoName)
     const videoPath = path.join(__dirname, 'uploads', videoName);
 
     // Kiểm tra nếu file video tồn tại
